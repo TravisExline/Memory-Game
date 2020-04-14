d2405c0ca53652dc452b25e5b40dc5298eebc7ef
travis_exline <travisexline@Ellyn-PC.localdomain>
2020-04-14 11:02:07 -0500

    can only flip first

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,1 +1,25 @@
+const playingCard = document.querySelector(".playing-card")
+const cardFront = document.querySelector(".card-front")
+const cardBack = document.querySelector(".card-back")
 
+function hideBacks() {
+  cardBack.style.display = "none"
+}
+
+function showBacks() {
+    cardBack.style.display = ""
+}
+
+function hideFronts() {
+    cardFront.style.display = "none"
+}
+
+function showFronts() {
+    cardFront.style.display = ""
+}
+
+playingCard.addEventListener('click', function(e) {
+    e.preventDefault()
+    hideBacks()
+    showFronts()
+})
