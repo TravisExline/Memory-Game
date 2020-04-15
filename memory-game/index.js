8fd1b44675c1c53bafada67704f0e0e21dc17888
travis_exline <travisexline@Ellyn-PC.localdomain>
2020-04-15 09:19:15 -0500

    working on match

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,20 +1,15 @@
-const playingCard = document.querySelector(".playing-card")
-const cardFront = document.querySelector(".card-front")
-const cardBack = document.querySelector(".card-back")
+const playingCards = document.querySelectorAll('.playing-card')
 
-function hideBacks() {
-  cardBack.style.display = "none"
+let hasFlipped = false
+let cardOne, cardTwo
+
+function flipCard() {
+    this.classList.toggle('flip');
+
+    if(!hasFlipped) {
+        hasFlipped = true;
+        cardOne = this
+    }
 }
 
-function showBacks() {
-    cardBack.style.display = ""
-}
-
-function hideFronts() {
-    cardFront.style.display = "none"
-}
-
-function showFronts() {
-    cardFront.style.display = ""
-}
-
+playingCards.forEach(card => card.addEventListener('click', flipCard))
