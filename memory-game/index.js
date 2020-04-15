36ade26e4a4f1725bece664f67570694001555e7
travis_exline <travisexline@Ellyn-PC.localdomain>
2020-04-15 09:29:20 -0500

    added return for flipCard()

--- memory-game/index.js
+++ memory-game/index.js
@@ -4,17 +4,20 @@ let hasFlipped = false
 let cardOne, cardTwo
 
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
 
     if(!hasFlipped) {
         hasFlipped = true;
         cardOne = this
+        // must return to allow second card to be flipped
+        return
     }
 
     isMatch()
 }
 
 function isMatch() {
+    debugger
     if(cardOne.id === cardTwo.id) {
         correctCards()
     }
